8ec06032757549203764594970181db000234fbd
kmuralikum <kmuralikum2002@gmail.com>
2024-03-13 16:20:26 +0530

    🦄 refactor: update telegram.service.js

--- services/telegram.service.js
+++ services/telegram.service.js
@@ -5,11 +5,11 @@ const TELEGRAM_CHAT_ID = "-4198026216"
 function TelegramService(){
     return {
         sendEnquiry:async (_)=>{
-            const response = await axios.get(`https://api.telegram.org/bot${TELEGRAM_BOT_ID}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=Enquiry%3A%0AName%3A${_?.name}%0APhone%3A${_?.phone}%0ANo.Of.People%3A${_?.peopleCount}%0APackage%3A${_?.package}%0AVehicle%3A${_?.vehicle}%0ATrip-Date%3A%${_?.tripDate}`)
+            const response = await axios.get(`https://api.telegram.org/bot${TELEGRAM_BOT_ID}/sendMessage?chat_id=${TELEGRAM_CHAT_ID}&text=Enquiry%0AName%3A${_?.name}%0APhone%3A${_?.phone}%0ANo.Of.People%3A${_?.peopleCount}%0APackage%3A${_?.package}%0AVehicle%3A${_?.vehicle}%0ATrip-Date%3A${_?.tripDate}`)
             .catch(err=>{
                 console.log(err);
             })
-            console.log(response);
+            // console.log(response);
         }
     }
 }
